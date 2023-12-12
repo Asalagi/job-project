3509cdf106f58f87402cd7f4838165cbdf2b4ac1
Asalagi <mypaintedfrog@yahoo.com>
2023-12-12 15:11:20 -0600

    adding form

--- jobjob-binks/src/App.js
+++ jobjob-binks/src/App.js
@@ -9,6 +9,21 @@ function App() {
         <div className="main-content">
           <div className="text-content">
             <h2 className="center">May the work force be with you!</h2>
+            <div className="signup-box">
+              <h3>Sign Up</h3>
+               <div className="signup-inner">
+                <div className="signup-label">
+                <label>Name</label><br/>
+                <label>Email</label><br/>
+                <label>Password</label><br/>
+                </div>
+                <div className="signup-input">
+                <input></input><br/>
+                <input></input><br/>
+                <input></input><br/>
+                </div>
+               </div>
+            </div>
           </div>
         </div>
       </div>
