1e861f5206756f2c7c217783247734128ba92b09
Asalagi <mypaintedfrog@yahoo.com>
2023-12-28 14:49:29 -0600

    add scenerios

--- adventurer/script.js
+++ adventurer/script.js
@@ -1,13 +1,22 @@
 let start = document.getElementById('start');
 let hike = document.getElementById('hike');
 let display = document.getElementById('display');
+let btnDisplay = document.getElementById('btn-display')
 let left = document.getElementById('left');
 let right = document.getElementById('right');
+let ignore = document.getElementById('ignore');
+let investigate = document.getElementById('investigate');
+
+let phrases = [
+    "You keep walking down the trail. The sun is shining and the birds are happily chirping.", 
+    "The day is beautiful and you are enjoying the wonderful outdoors. You take a deep breath and take in thr beauty or your surrounds.",
+    "You walk down the trail happy to be out in nature. What beautiful scenery!"
+]
 
 start.addEventListener('click', function () {
     start.classList.add('hidden');
     hike.classList.remove('hidden');
-    display.innerText = "You keep walking down the trail. The sun is shining and the birds are happily chirping."
+    display.innerText = phrases[Math.trunc(Math.random() * phrases.length)];
 });
 
 hike.addEventListener('click', function() {
@@ -17,8 +26,14 @@ hike.addEventListener('click', function() {
         left.classList.remove('hidden');
         right.classList.remove('hidden');
         hike.classList.add('hidden');
-    } else {
+    } else if (odds % 9 === 0) {
+        display.innerText = "You are happily walking down your trail until you hear a sound. Something is in the bushes. Would do you do?";
+        ignore.classList.remove('hidden');
+        investigate.classList.remove('hidden');
+        hike.classList.add('hidden');
+     } else {
         hike.classList.remove('hidden');
+        display.innerText = phrases[Math.trunc(Math.random() * phrases.length)];
     }
     console.log(odds);
 });
@@ -37,7 +52,20 @@ right.addEventListener('click', function() {
     right.classList.add('hidden');
 });
 
-let phrases = []
+ignore.addEventListener('click', function() {
+    display.innerText = "You ignore the sound and keep walking with a little more pep in your step."
+    hike.classList.remove('hidden');
+    ignore.classList.add('hidden');
+    investigate.classList.add('hidden');
+});
+
+investigate.addEventListener('click', function() {
+    display.innerText = "You walk toward the bushes with caution. You think 'It can't be that bad, maybe just a little squirel'. You peak throught the bushes... OH NO! It's a big furry bear. You scream, it charges! Flailing your arms in legs to get away, it catches up and eats you. You are no more and the Bear had a good snack."
+    hike.classList.add('hidden');
+    ignore.classList.add('hidden');
+    investigate.classList.add('hidden');
+    btnDisplay.innerText = "GAME OVER!"
+});
 
 /* if 'start' is clicked remove 'start' display 'hike' 
 when 'hike' is clicked randomize
